7c5386c56e243db897c8026e618b3f943ec520c9
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2026-07-09 01:22:16 -0700

    Added some lists

---
+++ src/Response/BillResponseReferralList.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * LegiScan (https://legiscan.com) Http Client
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Patrick Gunsolley <patrick.gunsolley@outlook.com>
+ * @copyright     Copyright (c) 2026 Patrick Gunsolley
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+declare(strict_types=1);
+
+namespace PGunsolley\LegiScan\Http\Response;
+
+use IteratorAggregate;
+use Traversable;
+use Override;
+
+class BillResponseReferralList implements IteratorAggregate
+{
+    public function __construct(private array $data)
+    {
+    }
+
+    #[Override]
+    public function getIterator(): Traversable
+    {
+        foreach ($this->data as $item) {
+            yield new BillResponseReferral(
+                date: $item['date'],
+                committeeId: $item['committee_id'],
+                chamber: $item['chamber'],
+                chamberId: $item['chamber_id'],
+                name: $item['name'],
+            );
+        }
+    }
+}
---
+++ src/Response/BillResponseSastList.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * LegiScan (https://legiscan.com) Http Client
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Patrick Gunsolley <patrick.gunsolley@outlook.com>
+ * @copyright     Copyright (c) 2026 Patrick Gunsolley
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+declare(strict_types=1);
+
+namespace PGunsolley\LegiScan\Http\Response;
+
+use IteratorAggregate;
+use Traversable;
+use Override;
+
+class BillResponseSastList implements IteratorAggregate
+{
+    public function __construct(private array $data)
+    {
+    }
+
+    #[Override]
+    public function getIterator(): Traversable
+    {
+        foreach ($this->data as $item) {
+            yield new BillResponseSast(
+                typeId: $item['type_id'],
+                type: $item['type'],
+                sastBillNumber: $item['sast_bill_number'],
+                sastBillId: $item['sast_bill_id'],
+            );
+        }
+    }
+}
